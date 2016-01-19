e841b204bed67dcba65a3c921e65d301a65f69d9
cluchi <cluchi@gmail.com>
2016-01-19 23:05:01 +0200

    Added test for 'medoo' mysql DB connection route /get/:id

--- public/index.php
+++ public/index.php
@@ -46,5 +46,37 @@ $app->get('/who/:name', function ($name) use ($app) {
 	
 });
 
+$app->get('/get/:id', function ($id) use ($app) {
+	//DB connection with medoo 
+	// TODO move to init file with config
+	$database = new medoo([
+		// required
+		'database_type' => 'mysql',
+		'database_name' => 'api',
+		'server' => '127.0.0.1',
+		'username' => 'root',
+		'password' => '',
+		'charset' => 'utf8',
+	 
+		// [optional]
+		'port' => 3306,
+	 
+		// [optional] Table prefix
+		//'prefix' => 'socialApp_',
+	 
+		// driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
+		'option' => [
+			PDO::ATTR_CASE => PDO::CASE_NATURAL
+		]
+	]);
+	$datas = $database->select("users", [
+		"username",
+		"email"
+	], [
+		"id" => $id
+	]);
+	$app->response->headers->set('Content-Type', 'application/json');
+	$app->response->body(json_encode($datas));
+});
 // Run app
 $app->run();
